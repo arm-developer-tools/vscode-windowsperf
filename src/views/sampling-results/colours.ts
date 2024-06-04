3282e02649e354508513ddaacb6b89fc7b30808c
Christian Jardine <christian.s.jardine@gmail.com>
2024-06-04 10:21:55 +0100

    Add Transparency value (#65)
    * Add 40% opacity
    
    * up opacity to 0.6
    
    * adjust to 30%

--- src/views/sampling-results/colours.ts
+++ src/views/sampling-results/colours.ts
@@ -6,5 +6,7 @@ import * as d3 from 'd3';
 
 export const textEditorColour = (overhead: number): string => {
     const colorScale = d3.scaleSequential([0, 100], d3.interpolateTurbo);
-    return colorScale(overhead);
+    const colour = colorScale(overhead);
+    const opacity = '0.3';
+    return `${colour.slice(0, -1)}, ${opacity})`;
 };
