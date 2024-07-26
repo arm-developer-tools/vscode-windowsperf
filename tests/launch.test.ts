56ea57d777205f07b055a95c6cdb438870e0d622
Jamie Wilson <jamie.wilson@arm.com>
2024-07-26 10:41:28 +0100

    Extracted variables for keyboard shortcuts

--- tests/launch.test.ts
+++ tests/launch.test.ts
@@ -4,9 +4,12 @@ import path from 'node:path';
 test.describe('Loading the WindowsPerf extension', () => {
     test.use({ extensionDevelopmentPath: path.resolve(__dirname, '../../') });
 
+    const toggleSidebarShortcut = 'ControlOrMeta+B';
+    const toggleBottomPanelShortcut = 'ControlOrMeta+J';
+
     test('a WindowsPerf tab appears in the bottom panel', async ({ vscode }) => {
-        await vscode.page.keyboard.press('ControlOrMeta+B'); // Closes the sidebar
-        await vscode.page.keyboard.press('ControlOrMeta+J'); // Opens the bottom panel
+        await vscode.page.keyboard.press(toggleSidebarShortcut);
+        await vscode.page.keyboard.press(toggleBottomPanelShortcut);
         await vscode.page.getByText('WindowsPerf').waitFor();
         expect(vscode.page.getByText('WindowsPerf')).toBeVisible();
     });
