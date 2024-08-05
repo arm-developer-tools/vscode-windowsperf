dca05417ecfa3312a546bfe65410fe4c56f0e010
Matthew Gordon <matthew.gordon@arm.com>
2024-08-05 12:30:48 +0100

    Add factory for form props. (#215)

--- src/views/sampling-settings/components/form.test.tsx
+++ src/views/sampling-settings/components/form.test.tsx
@@ -6,45 +6,37 @@ import 'jest';
 import * as React from 'react';
 import { fireEvent, render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
-import { Form } from './form';
+import { Form, FormProps } from './form';
 import { recordOptionsFactory } from '../../../wperf/record-options.factories';
 import { UpdateRecordOptionAction } from '../state/update-record-option-action';
+import { predefinedEventFactory } from '../../../wperf/parse/list.factories';
+import { ValidatedField } from '../../../wperf/record-options';
+
+const formPropsFactory = (options?: Partial<FormProps>): FormProps => ({
+    cores: options?.cores ?? [],
+    events: options?.events ?? [predefinedEventFactory()],
+    recordOptions: options?.recordOptions ?? recordOptionsFactory(),
+    fieldsToValidate: options?.fieldsToValidate ?? [],
+    record: options?.record ?? jest.fn(),
+    openCommandFilePicker: options?.openCommandFilePicker ?? jest.fn(),
+    updateRecordOption: options?.updateRecordOption ?? jest.fn(),
+});
 
 describe('Form', () => {
     it('calls updateRecordOption when the command input changes', () => {
-        const updateRecordOption = jest.fn();
-        render(
-            <Form
-                cores={[]}
-                events={[]}
-                recordOptions={recordOptionsFactory()}
-                record={jest.fn()}
-                openCommandFilePicker={jest.fn()}
-                updateRecordOption={updateRecordOption}
-                fieldsToValidate={[]}
-            />,
-        );
+        const props = formPropsFactory();
+        render(<Form {...props} />);
 
         const newCommand = 'new-command';
         fireEvent.change(screen.getByTestId('command-input'), { target: { value: newCommand } });
 
         const want: UpdateRecordOptionAction = { type: 'setCommand', command: newCommand };
-        expect(updateRecordOption).toHaveBeenCalledWith(want);
+        expect(props.updateRecordOption).toHaveBeenCalledWith(want);
     });
 
     it('calls updateRecordOption when the arguments input changes', () => {
-        const updateRecordOption = jest.fn();
-        render(
-            <Form
-                cores={[]}
-                events={[]}
-                recordOptions={recordOptionsFactory()}
-                record={jest.fn()}
-                openCommandFilePicker={jest.fn()}
-                updateRecordOption={updateRecordOption}
-                fieldsToValidate={[]}
-            />,
-        );
+        const props = formPropsFactory();
+        render(<Form {...props} />);
 
         const newArguments = '-some -arguments';
         fireEvent.change(screen.getByTestId('arguments-input'), {
@@ -52,108 +44,55 @@ describe('Form', () => {
         });
 
         const want: UpdateRecordOptionAction = { type: 'setArguments', arguments: newArguments };
-        expect(updateRecordOption).toHaveBeenCalledWith(want);
+        expect(props.updateRecordOption).toHaveBeenCalledWith(want);
     });
 
     it('renders the event selection', () => {
-        render(
-            <Form
-                cores={[]}
-                events={[]}
-                recordOptions={recordOptionsFactory()}
-                record={jest.fn()}
-                openCommandFilePicker={jest.fn()}
-                updateRecordOption={jest.fn()}
-                fieldsToValidate={[]}
-            />,
-        );
+        render(<Form {...formPropsFactory()} />);
 
         expect(screen.queryByText('Events', { selector: 'h2' })).toBeInTheDocument();
     });
 
     it('renders the CPU Core selection', () => {
-        render(
-            <Form
-                cores={[]}
-                events={[]}
-                recordOptions={recordOptionsFactory()}
-                record={jest.fn()}
-                openCommandFilePicker={jest.fn()}
-                updateRecordOption={jest.fn()}
-                fieldsToValidate={[]}
-            />,
-        );
+        render(<Form {...formPropsFactory()} />);
 
         expect(screen.queryByText('CPU Core', { selector: 'h2' })).toBeInTheDocument();
     });
 
     it('calls openCommandFilePicker when the browse button is clicked', () => {
-        const openCommandFilePicker = jest.fn();
-        render(
-            <Form
-                cores={[]}
-                events={[]}
-                recordOptions={recordOptionsFactory()}
-                record={jest.fn()}
-                openCommandFilePicker={openCommandFilePicker}
-                updateRecordOption={jest.fn()}
-                fieldsToValidate={[]}
-            />,
-        );
+        const props = formPropsFactory();
+        render(<Form {...props} />);
 
         fireEvent.click(screen.getByText('Browse'));
 
-        expect(openCommandFilePicker).toHaveBeenCalled();
+        expect(props.openCommandFilePicker).toHaveBeenCalled();
     });
 
     it('calls record when the record event button is clicked', () => {
-        const record = jest.fn();
-        render(
-            <Form
-                cores={[]}
-                events={[]}
-                recordOptions={recordOptionsFactory()}
-                record={record}
-                openCommandFilePicker={jest.fn()}
-                updateRecordOption={jest.fn()}
-                fieldsToValidate={[]}
-            />,
-        );
+        const props = formPropsFactory();
+        render(<Form {...props} />);
 
         const recordButton = screen.getAllByText('Record');
         fireEvent.click(recordButton[0]!);
 
-        expect(record).toHaveBeenCalled();
+        expect(props.record).toHaveBeenCalled();
     });
 
     it('does not render the command input with an invalid class when the command is missing but command is not a field to validate', () => {
-        render(
-            <Form
-                cores={[]}
-                events={[]}
-                recordOptions={recordOptionsFactory({ command: '' })}
-                openCommandFilePicker={jest.fn()}
-                record={jest.fn()}
-                updateRecordOption={jest.fn()}
-                fieldsToValidate={[]}
-            />,
-        );
+        const recordOptions = recordOptionsFactory({ command: '' });
+        const props = formPropsFactory({ recordOptions });
+
+        render(<Form {...props} />);
 
         expect(screen.getByTestId('command-input')).not.toHaveClass('invalid');
     });
 
     it('renders the command input with an invalid class when the command is missing and command is a field to validate', () => {
-        render(
-            <Form
-                cores={[]}
-                events={[]}
-                recordOptions={recordOptionsFactory({ command: '' })}
-                openCommandFilePicker={jest.fn()}
-                record={jest.fn()}
-                updateRecordOption={jest.fn()}
-                fieldsToValidate={['command']}
-            />,
-        );
+        const fieldsToValidate: ValidatedField[] = ['command'];
+        const recordOptions = recordOptionsFactory({ command: '' });
+        const props = formPropsFactory({ recordOptions, fieldsToValidate });
+
+        render(<Form {...props} />);
 
         expect(screen.getByTestId('command-input')).toHaveClass('invalid');
     });
