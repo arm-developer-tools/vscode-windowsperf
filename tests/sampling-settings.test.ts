73b8f59b72adc81fdc98d71c4ccd71cd325fc203
Christian Jardine <christian.s.jardine@gmail.com>
2024-08-12 14:53:40 +0100

    E2E for sample setting for error (#220)
    * draft
    
    * test: add E2E test for sampling settings error
    
    * test: refactor
    
    * test: refactor text string shorter
    
    * test: increase timeout for windows
    
    * test: up timing for windows
    
    * refactor: remove the waitFor
    
    * chore: remove import
    
    * test: slow the test
    
    * chore: remove launch test
    
    * test: try something to extend time
    
    * refactor: add await
    
    * chore: test updating the CI on branch with WOA
    
    * CI: set ubuntu only flag to true
    
    * test: remove extended timeout
    
    * CI: set back to use versioned flow
    
    * CI: set the right thing
    
    * revert back
    
    * run ubuntu latest only
    
    * test on branch with xvfb removed
    
    * bump version
    
    * sync lock file
    
    * chore: add coverage ignore path
    
    * refactor: add suggested changes
    
    * refactor: ignore playwright config file
    
    * chore: add missing ignore symbol
    
    * exclude
    
    * update flags
    
    * Update package.json
    
    Co-authored-by: Matthew Gordon <matthew.gordon@arm.com>
    
    ---------
    
    Co-authored-by: Matthew Gordon <matthew.gordon@arm.com>

---
+++ tests/sampling-settings.test.ts
@@ -0,0 +1,24 @@
+/*
+ * Copyright (c) 2024 Arm Limited
+ */
+
+import { test, expect } from '@arm-debug/vscode-playwright-test';
+import path from 'node:path';
+
+test.describe('Sampling Settings', () => {
+    test.use({ extensionDevelopmentPath: path.resolve(__dirname, '../../') });
+
+    const toggleSidebarShortcut = 'ControlOrMeta+B';
+    const toggleBottomPanelShortcut = 'ControlOrMeta+J';
+
+    test('opens the sampling setting with the error display', async ({ vscode }) => {
+        await vscode.page.keyboard.press(toggleSidebarShortcut);
+        await vscode.page.keyboard.press(toggleBottomPanelShortcut);
+        await vscode.page.getByText('WindowsPerf').click();
+        await vscode.page.getByRole('button', { name: 'Show Sampling Settings' }).click();
+        const parentFrame = vscode.page.frameLocator('iframe[class="webview ready"]');
+        const childFrame = parentFrame.frameLocator('#active-frame');
+
+        await expect(childFrame.getByText('Command failed:')).toBeVisible();
+    });
+});
